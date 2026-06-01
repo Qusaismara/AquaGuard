b9158732ab87c06b669ded7fb55d5b996c4e3377
ahmadnazzal379 <anazzal201@gmail.com>
2026-06-01 20:24:39 +0300

    Add pool loading function

--- pool_manager.py
+++ pool_manager.py
@@ -1,4 +1,13 @@
 import json
 
 class PoolManager:
-   
+
+    def load_pools(self, filepath: str) -> list:
+
+        try:
+            with open(filepath, "r", encoding="utf-8") as f:
+                data = json.load(f)
+                return data["pools"]
+
+        except FileNotFoundError:
+            raise FileNotFoundError(f"File not found: {filepath}")
